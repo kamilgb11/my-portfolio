882557f8892e71d7efca92c4ebd95cf9a5277111
kamilgb11 <163057857+kamilgb11@users.noreply.github.com>
2024-10-23 13:49:09 +0200

    respons

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -7,7 +7,7 @@ function WelcomeSection() {
         <p>Hello! <br /> I am Frontend Developer</p>
       </div>
       <div className="message">
-        <img src="https://cdn.pixabay.com/photo/2017/06/07/13/53/binary-2380422_1280.jpg" alt="" />
+      <img src="https://cdn.pixabay.com/photo/2017/06/07/13/53/binary-2380422_1280.jpg" alt="" />
       </div>
     </section>
   );
